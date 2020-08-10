6ecf4f1470972b79117676a51a0314ba31b2d4b5
Jorge Cardoso Rabello <jorgecardosorabello@gmail.com>
2020-08-11 00:16:14 +0200

    Front end Completed

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,37 +1,59 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem() {
-  return(
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
+  return (
     <article className="teacher-item">
-    <header>
-      <img src="https://avatars3.githubusercontent.com/u/50869604?s=460&u=a20451306862765c59a3b7593ad147654a1e0265&v=4" alt="Jorge Cardoso Rabello"/>
-      <div>
-        <strong>Jorge Cardoso Rabello</strong>
-        <span>AutoCad</span>
-      </div>
-    </header>
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
 
-    <p>
-    Entusiasta das melhores tecnologias de química avançada.
-    <br /><br />
-    Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-    </p>
+      <p>{teacher.bio}</p>
 
-    <footer>
-      <p>
-        Preço/hora
-        <strong>R$ 80,00</strong>
-      </p>
-      <button type="button">
-        <img src={whatsappIcon} alt="Whatsapp Icon"/>
-        Entrar em contato
-      </button>
-    </footer>
-  </article>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+        <a 
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
   );
 }
 
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -8,20 +8,32 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data; 
+
+      setTotalConnections(total);
+    })
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
         <div className="logo-container">
-          <img src={logoImg} alt="Proffy"/>
+          <img src={logoImg} alt="Proffy" />
           <h2>Sua plataforma de estudos online.</h2>
         </div>
 
-        <img
-          src={landingImg}
-          alt="Plataforma de estudos"
+        <img 
+          src={landingImg} 
+          alt="Plataforma de estudos" 
           className="hero-image"
         />
 
@@ -38,7 +50,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+          Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
